a9d3a7f75c146a710c62ab23198fec4fda7847db
joryte <joryte@c8a4d5ff-d926-0410-a673-d1d1d7019e50>
2007-03-24 06:46:58 +0000

    Modified to allow receiving and handling of different message types whose implementations will have to be determined later.

--- trunk/Battleship/src/Networking/ThreadedReceiver.java
+++ trunk/Battleship/src/Networking/ThreadedReceiver.java
@@ -35,8 +35,28 @@ public class ThreadedReceiver extends Thread {
 		    	try {
 		    			// Wait for a message to arrive
 					while( (data=server.in.readUTF()) == null );
-						// Display message
-					System.out.println(server.GetClientName() + ": " + data);
+					
+						// Switch the message type
+					switch(data.charAt(0)){			
+					case 'B':		// Board Data
+						// call the string to board conversion function
+						// will probably need to save a reference of the
+						// object that contains the function
+						break;						
+					case 'C':		// Coordinate Data
+						// call the string to coordinate converstion function
+						// will probably need to save a reference of the 
+						// object tat contains the function
+						break;					
+					case 'S':		// System Data
+						// figure out what system messages might come and
+						// determine how to handle them here
+						break;	
+					case 'M':		// Chat Message	
+						default:   	//  use as default, if all else fails, print it!
+							// Display message
+							System.out.println(server.GetClientName() + ": " + data.substring(1));	
+					}
 		        } catch (IOException e) {
 					System.out.println( "IO: "+e.getMessage() );
 		        }
@@ -46,7 +66,28 @@ public class ThreadedReceiver extends Thread {
 			while(client.in != null){
 		    	try {
 					while( (data=client.in.readUTF()) == null );
-					System.out.println(client.GetServerName() + ": " + data);
+					
+					// Switch the message type
+					switch(data.charAt(0)){			
+					case 'B':		// Board Data
+						// call the string to board conversion function
+						// will probably need to save a reference of the
+						// object that contains the function
+						break;						
+					case 'C':		// Coordinate Data
+						// call the string to coordinate converstion function
+						// will probably need to save a reference of the 
+						// object tat contains the function
+						break;					
+					case 'S':		// System Data
+						// figure out what system messages might come and
+						// determine how to handle them here
+						break;	
+					case 'M':		// Chat Message	
+						default:   	//  use as default, if all else fails, print it!
+							// Display message
+							System.out.println(client.GetServerName() + ": " + data.substring(1));
+					}
 		        } catch (IOException e) {
 					System.out.println( "IO: "+e.getMessage() );
 		        }
